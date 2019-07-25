21cf6ad7f8f2948751191a7355688b60463693f6
greg9702 <grzegorzb9702@gmail.com>
2019-07-25 11:03:26 +0200

    fixed IP=none bug in cluster create script

--- cluster/setup.sh
+++ cluster/setup.sh
@@ -2,11 +2,23 @@
 
 export PATH=$PATH:$(go env GOPATH)/bin
 
-kind delete cluster
-kind create cluster --config cluster-config.yaml
+if [ "$1" != "" ]; then
+    cluster_name=$1
+else
+    cluster_name="kind"
+fi
 
+if [ "$2" != "" ]; then
+    echo "Passed to many arguments"
+	echo -e "usage:\n./setup.sh 'cluster name' "
+	exit 1
+fi
 
-export KUBECONFIG="$(kind get kubeconfig-path --name="kind")" # TODO cluster name make it as variable
+kind delete cluster --name=$cluster_name
+kind create cluster --config cluster-config.yaml --name=$cluster_name
+
+export KUBECONFIG="$(kind get kubeconfig-path --name=$cluster_name)"
+
 # if set to 1, create dashboard
 create_dashboard=1
 
@@ -16,16 +28,17 @@ create_admin=1
 if [ $create_dashboard == 1 ]; then
     echo "Creating dashboard..."
 	kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.0-beta1/aio/deploy/recommended.yaml
+	#we have to wait to assign ip address to this pod TODO do while loop
+	sleep 5
 	# change dashboard type to NodePort
 	kubectl patch svc -n kubernetes-dashboard kubernetes-dashboard --type='json' -p '[{"op":"replace","path":"/spec/type","value":"NodePort"}]'
-	echo "changed type"
-	# address and port of node on which dashboard is running
-	tmp_address=$(kubectl get nodes -o wide | grep "$(kubectl get pods -o wide --all-namespaces | grep dashboard- |  cut -d" " -f 37)" | cut -d" " -f17) #TODO change this cut to awk
-	echo "got address"
-	tmp_port=$(kubectl -n kubernetes-dashboard get service kubernetes-dashboard  |  awk 'FNR == 2 {print $5}' | cut -d":" -f 2 | cut -d"/" -f 1)
-	echo "got port"
 
-	echo -e "Dashboard can be accesed at https://$tmp_address:$tmp_port"
+	# address and port of node on which dashboard is running
+	node_name=$(kubectl get pods -o wide --all-namespaces | grep dashboard- | awk '{print $8}')
+	tmp_address=$(kubectl get nodes -o wide | grep "$(echo $node_name)" | awk '{print $6}')
+	tmp_port=$(kubectl -n kubernetes-dashboard get service kubernetes-dashboard  |  awk 'FNR == 2 {print $5}' | cut -d":" -f 2 | cut -d"/" -f 1)
+
+	echo -e "Dashboard is running at https://$tmp_address:$tmp_port"
 else
     echo "Dashboard not created"
 fi
