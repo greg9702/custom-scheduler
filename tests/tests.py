282ba7b5e3366b88922cfaf113e6a16de84f1163
greg9702 <grzegorzb9702@gmail.com>
2019-08-05 13:30:22 +0200

    changed module import to relative path

--- tests/tests.py
+++ tests/tests.py
@@ -1,8 +1,13 @@
 import pytest
 
-@pytest.mark.webtest
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '../pkg/scheduler/src'))
+from scheduler import Scheduler
+
+@pytest.mark.scheduler
 class TestClass(object):
 	def test_startup(self):
-		pass
+		self.scheduler = Scheduler()
+
 	def test_startup_and_more(self):
 		pass
