3de2d5701ff02c794ab64bda343483a21512bf6c
greg9702 <grzegorzb9702@gmail.com>
2019-08-06 07:02:57 +0200

    back off

--- tests/fake_cluster.py
+++ tests/fake_cluster.py
@@ -1,5 +1,31 @@
 import json
 from pprint import pprint
+from bunch import bunchify
+
+nodes_params = [{'metadata': { 'labels': {'beta.kubernetes.io/arch': 'amd64',
+                         'beta.kubernetes.io/os': 'linux',
+                         'kubernetes.io/arch': 'amd64',
+                         'kubernetes.io/hostname': 'kind-control-plane',
+                         'kubernetes.io/os': 'linux',
+                         'node-role.kubernetes.io/master': ''},
+              'name': 'kind-control-plane'},
+ 'spec': {'taints': [{'effect': 'NoSchedule',
+                      'key': 'node-role.kubernetes.io/master',
+                      'time_added': None,
+                      'value': None}]},
+          'unschedulable': None,
+ 'status': {'allocatable': {'cpu': '4',
+                            'ephemeral-storage': '479177440Ki',
+                            'hugepages-2Mi': '0',
+                            'memory': '8045056Ki',
+                            'pods': '110'},
+            'capacity': {'cpu': '4',
+                         'ephemeral-storage': '479177440Ki',
+                         'hugepages-2Mi': '0',
+                         'memory': '8045056Ki',
+                         'pods': '110'}},
+ 'usage': {'cpu': '253011429n', 'memory': '698744Ki'}}]
+
 class fakeCluster:
 	def __init__(self, nodes_params):
 		'''
@@ -18,43 +44,14 @@ class fakeCluster:
 		'''
 		template = {}
 		try:
-			with open('node_template.json', 'r') as f:
-				template = json.load(f)
+			with open('node_template.txt', 'r') as f:
+				# toplate = f
 		except Exception as e:
 			pprint(str(e))
-		# pprint(json_node)
-		pprint(template.usage)
-		# for node in nodes_params:
-			# json_node = json.loads(node)
-			# for element in json_node:
-				# pprint(template.metadata)
 		return
 
-nodes_params = ['''{"metadata": { "labels": {"beta.kubernetes.io/arch": "amd64",
-                         "beta.kubernetes.io/os": "linux",
-                         "kubernetes.io/arch": "amd64",
-                         "kubernetes.io/hostname": "kind-control-plane",
-                         "kubernetes.io/os": "linux",
-                         "node-role.kubernetes.io/master": ""},
-              "name": "kind-control-plane"},
- "spec": {"taints": [{"effect": "NoSchedule",
-                      "key": "node-role.kubernetes.io/master",
-                      "time_added": "None",
-                      "value": "None"}]},
-          "unschedulable": "None",
- "status": {"allocatable": {"cpu": "4",
-                            "ephemeral-storage": "479177440Ki",
-                            "hugepages-2Mi": "0",
-                            "memory": "8045056Ki",
-                            "pods": "110"},
-            "capacity": {"cpu": "4",
-                         "ephemeral-storage": "479177440Ki",
-                         "hugepages-2Mi": "0",
-                         "memory": "8045056Ki",
-                         "pods": "110"}},
- "usage": {"cpu": "253011429n", "memory": "698744Ki"}}''']
-
 def main():
-	f = fakeCluster(nodes_params)
+	f = fakeCluster(nodes_params[0])
+
 if __name__ == '__main__':
     main()
