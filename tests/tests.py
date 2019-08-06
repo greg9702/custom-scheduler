8895dad3f8a037166360114cd8c6b55ff470e7c9
greg9702 <grzegorzb9702@gmail.com>
2019-08-06 08:44:25 +0200

    working on fixtures

--- tests/tests.py
+++ tests/tests.py
@@ -2,19 +2,20 @@ import pytest
 import os
 import sys
 
+from fakeCluster import fakeCluster
 sys.path.append(os.path.join(os.path.dirname(__file__), '../pkg/scheduler/src'))
 from scheduler import Scheduler
-from fake_cluster import fakeCluster
 
-nodes_params = [
-	{
-	"metadata": { "labels": {"beta.kubernetes.io/arch": "amd64",
+@pytest.fixture()
+def nodes_ficture():
+	nodes_params = [
+	'''{"metadata": { "labels": {"beta.kubernetes.io/arch": "amd64",
 	                         "beta.kubernetes.io/os": "linux",
 	                         "kubernetes.io/arch": "amd64",
 	                         "kubernetes.io/hostname": "kind-control-plane",
 	                         "kubernetes.io/os": "linux",
 	                         "node-role.kubernetes.io/master": ""},
-	              "name": "kind-control-plane",
+	              "name": "kind-control-plane"},
 	 "spec": {"taints": [{"effect": "NoSchedule",
 	                      "key": "node-role.kubernetes.io/master",
 	                      "time_added": "None",
@@ -29,17 +30,41 @@ nodes_params = [
 	                         "ephemeral-storage": "479177440Ki",
 	                         "hugepages-2Mi": "0",
 	                         "memory": "8045056Ki",
-	                         "pods": "110"},
-	 "usage": {"cpu": "253011429n", "memory": "698744Ki"}
-	 },
+	                         "pods": "110"}},
+	 "usage": {"cpu": "253011429n", "memory": "698744Ki"}}''',
 
-]
-@pytest.mark.scheduler
+	'''{"metadata": { "labels": {"beta.kubernetes.io/arch": "amd64",
+                         "beta.kubernetes.io/os": "linux",
+                         "kubernetes.io/arch": "amd64",
+                         "kubernetes.io/hostname": "worker-node",
+                         "kubernetes.io/os": "linux",
+                         "node-role.kubernetes.io/master": ""},
+              "name": "worker-node"},
+ "spec": {"taints": [{"effect": "NoSchedule",
+                      "key": "node-role.kubernetes.io/master",
+                      "time_added": "None",
+                      "value": "None"}],
+          "unschedulable": "None"},
+ "status": {"allocatable": {"cpu": "4",
+                            "ephemeral-storage": "479177440Ki",
+                            "hugepages-2Mi": "0",
+                            "memory": "8045056Ki",
+                            "pods": "110"},
+            "capacity": {"cpu": "4",
+                         "ephemeral-storage": "479177440Ki",
+                         "hugepages-2Mi": "0",
+                         "memory": "8045056Ki",
+                         "pods": "110"}},
+ "usage": {"cpu": "253011429n", "memory": "698744Ki"}}'''
+ ]
+
+@pytest.mark.usefixtures('nodes_ficture')
 class TestClass(object):
 	def initTest(self):
+		self.fakeCluster(nodes_params)
 		self.scheduler = Scheduler()
-		self.f_cluster = fakeCluster(nodes_params)
-
-
-	def test2(self):
+		self.scheduler.all_nodes = self.fakeCluster.fake_nodes
 		pass
+
+	# def test2(self):
+	# 	pass
