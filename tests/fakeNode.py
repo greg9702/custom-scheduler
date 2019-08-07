a950f7d6df6bea01437a0b5c6aa3ec876bc23402
greg9702 <grzegorzb9702@gmail.com>
2019-08-07 15:55:21 +0200

    added original class attributes snippet, deleted unnecessary attributes

--- tests/fakeNode.py
+++ tests/fakeNode.py
@@ -1,11 +1,19 @@
 from kubernetes import client
 
+'''
+	Original node class attributes
+
+	'api_version': 'str',
+    'kind': 'str',
+    'metadata': 'V1ObjectMeta',
+    'spec': 'V1NodeSpec',
+    'status': 'V1NodeStatus'
+'''
+
 class fakeNode():
 	def __init__(self):
 		client.models.v1_node.V1Node.swagger_types['usage'] = 'object'
 		client.models.v1_node.V1Node.attribute_map['usage'] = 'usage'
-		self.api_version = "fakenode"
-		self.kind = "fakenode"
 		self.metadata = client.models.V1ObjectMeta()
 		self.spec = client.models.V1NodeSpec()
 		self.status = client.models.V1NodeStatus()
