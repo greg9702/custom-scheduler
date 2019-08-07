23c6c9e50b59d3bb92abc1bf076b33d9febe3b1e
greg9702 <grzegorzb9702@gmail.com>
2019-08-07 15:55:47 +0200

    creating fake pod module

---
+++ tests/fakePod.py
@@ -0,0 +1,18 @@
+from kubernetes import client
+
+'''
+	Original pod class attributes
+
+	'api_version': 'str',
+    'kind': 'str',
+    'metadata': 'V1ObjectMeta',
+    'spec': 'V1PodSpec',
+    'status': 'V1PodStatus'
+'''
+
+class fakePod:
+	def __init__(self):
+		self.metadata = client.models.V1ObjectMeta()
+		self.spec = client.models.V1PodSpec()
+		self.status = client.models.V1PodStatus()
+		pass
