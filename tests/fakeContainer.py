abb146ed7acf39b15592abe02463a728e64c974b
greg9702 <grzegorzb9702@gmail.com>
2019-08-09 21:21:08 +0200

    added original class describtion

--- tests/fakeContainer.py
+++ tests/fakeContainer.py
@@ -1,5 +1,31 @@
 from kubernetes import client
 
+'''
+    Original containter class
+
+    'args': 'list[str]',
+    'command': 'list[str]',
+    'env': 'list[V1EnvVar]',
+    'env_from': 'list[V1EnvFromSource]',
+    'image': 'str',
+    'image_pull_policy': 'str',
+    'lifecycle': 'V1Lifecycle',
+    'liveness_probe': 'V1Probe',
+    'name': 'str',
+    'ports': 'list[V1ContainerPort]',
+    'readiness_probe': 'V1Probe',
+    'resources': 'V1ResourceRequirements',
+    'security_context': 'V1SecurityContext',
+    'stdin': 'bool',
+    'stdin_once': 'bool',
+    'termination_message_path': 'str',
+    'termination_message_policy': 'str',
+    'tty': 'bool',
+    'volume_devices': 'list[V1VolumeDevice]',
+    'volume_mounts': 'list[V1VolumeMount]',
+    'working_dir': 'str'
+'''
+
 class fakeContainer:
     def __init__(self, name = "", limits = {}, requests = {}):
         self.name = name
