068068f1ba3358daa940b696b20de7537cf79b18
greg9702 <grzegorzb9702@gmail.com>
2019-08-09 14:49:41 +0200

    finished addPods method

--- tests/fakePodList.py
+++ tests/fakePodList.py
@@ -1,3 +1,6 @@
+import json
+import pod_template
+import fakeContainer
 from kubernetes import client
 from fakePod import fakePod
 
@@ -11,11 +14,30 @@ from fakePod import fakePod
         'metadata': 'V1ListMeta'
 '''
 
-class fakePod:
+class fakePodList:
 	def __init__(self):
 		self.items = []
 		pass
 
 	def addPods(self, pods_params):
-		
-		pass
+		for pod in pods_params:
+			fake_pod = fakePod()
+			json_pod = json.loads(pod)
+			fake_pod.spec.containers = []
+			fake_pod.metadata.generate_name = json_pod['metadata']['generate_name']
+			fake_pod.metadata.labels = json_pod['metadata']['labels']
+			fake_pod.spec.affinity = json_pod['spec']['affinity']
+			fake_pod.spec.node_name = json_pod['spec']['node_name']
+			fake_pod.spec.scheduler_name = json_pod['spec']['scheduler_name']
+			fake_pod.spec.tolerations = json_pod['spec']['tolerations']
+			fake_pod.status.phase = json_pod['status']['phase']
+			for el in json_pod['spec']['containers']:
+				fake_pod.spec.containers.append(fakeContainer.fakeContainer(el['name'], el['resources']['limits'], el['resources']['requests']))
+			self.items.append(fake_pod)
+
+if __name__ == '__main__':
+	f = fakePodList()
+	f.addPods(pod_template.pods_params)
+	for el in f.items:
+		for i in el.spec.containers:
+			print(i.name)
