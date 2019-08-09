112dd8c4cc6e2481d3f20a0443b8b8524a608ae0
greg9702 <grzegorzb9702@gmail.com>
2019-08-09 14:47:44 +0200

    removing redundant lines, no longer inherit from V1Node

--- tests/fakeNode.py
+++ tests/fakeNode.py
@@ -12,8 +12,6 @@ from kubernetes import client
 
 class fakeNode():
 	def __init__(self):
-		client.models.v1_node.V1Node.swagger_types['usage'] = 'object'
-		client.models.v1_node.V1Node.attribute_map['usage'] = 'usage'
 		self.metadata = client.models.V1ObjectMeta()
 		self.spec = client.models.V1NodeSpec()
 		self.status = client.models.V1NodeStatus()
